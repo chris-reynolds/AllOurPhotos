57fd59fc58182b45983d651673b5c0776b7efa8d
Chris Reynolds <chrisr@instantobjects.com>
2018-10-16 17:17:38 +1300

    add yearMonth constructor to ImageFilter

--- all_our_photos_app/lib/ImageFilter.dart
+++ all_our_photos_app/lib/ImageFilter.dart
@@ -7,18 +7,30 @@ import 'package:all_our_photos_app/ImgFile.dart';
 int _defaultMinRank = 1;
 int _defaultMaxRank = 2;
 
+typedef ImageFilterAction = void Function(ImageFilter);
+
 class ImageFilter {
 
 
   DateTime _fromDate = DateTime(1900);
   DateTime _toDate = DateTime(2030);
+  ImageFilterAction onRefresh;
   int _minRank = _defaultMinRank;
   int _maxRank = _defaultMaxRank;
   String _searchText = '';
   List<ImgFile> _images;
   bool _refreshRequired = true;
   // two constructors
-  ImageFilter.dateRange(this._fromDate, this._toDate);
+  ImageFilter.dateRange(this._fromDate, this._toDate,{ImageFilterAction refresh}) {
+    onRefresh = refresh;
+  } // create with dateRange
+
+  ImageFilter.yearMonth(int year,int month,{ImageFilterAction refresh}) {
+    _fromDate = DateTime(year,month,1);
+    _toDate = _fromDate.add(Duration(days: 31));
+    onRefresh = refresh;
+  }  // create with yearMonth
+
   ImageFilter.searchText(String searchText) {
     this.searchText = searchText;  // force lowercase
   }
@@ -49,6 +61,8 @@ class ImageFilter {
     // todo check ascending or descending date sort
     _images.sort((img1,img2) => img1.takenDate.difference(img2.takenDate).inMinutes);
     _refreshRequired = false;
+    if (onRefresh != null)  // alert listen of changes
+      onRefresh(this);
   } // of calcImages
 
   void extendDateRange({int days = 7}) {
