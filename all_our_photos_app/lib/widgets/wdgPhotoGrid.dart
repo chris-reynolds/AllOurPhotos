f758881258273771cfda73788d3d4205ffcdd55d
Chris Reynolds <chrisr@instantobjects.com>
2018-10-20 17:28:15 +1300

    Migrate from animation sample to filtered image list

--- all_our_photos_app/lib/widgets/wdgPhotoGrid.dart
+++ all_our_photos_app/lib/widgets/wdgPhotoGrid.dart
@@ -1,34 +1,41 @@
-// Copyright 2016 The Chromium Authors. All rights reserved.
-// Use of this source code is governed by a BSD-style license that can be
-// found in the LICENSE file.
+/*
+  Created by Chris on 20th Oct 2018
+
+  Purpose: Stateful ImageList widget
+*/
+
 
 import 'package:flutter/material.dart';
+import 'package:all_our_photos_app/ImageFilter.dart';
+import 'package:all_our_photos_app/widgets/wdgImageFilter.dart';
+import 'package:all_our_photos_app/widgets/ImageEditorWidget.dart';
+import 'package:all_our_photos_app/ImgFile.dart';
+import 'package:all_our_photos_app/srvCatalogueLoader.dart';
 
-enum GridDemoTileStyle {
-  imageOnly,
-  oneLine,
-  twoLine,
-  selecting
-}
-
-typedef void BannerTapCallback(Photo photo);
-
-const double _kMinFlingVelocity = 800.0;
-const String _kGalleryAssetsPackage = 'flutter_gallery_assets';
+typedef void BannerTapCallback(Photo photo,ImgFile imageFile);
 
 class Photo {
-  Photo({
-    this.assetName,
-    this.assetPackage,
-    this.title,
-    this.caption,
-    this.isFavorite = false,
-  });
+//  Photo({
+//    this.assetName,
+//    this.assetPackage,
+//    this.title,
+//    this.caption,
+//    this.isFavorite = false,
+//  });
 
-  final String assetName;
-  final String assetPackage;
-  final String title;
-  final String caption;
+  Photo.byImgFile(this._imageFile) {
+    this.assetName = thumbnailURL(_imageFile);
+    this.title = _imageFile.location;
+    this.caption = _imageFile.caption;
+    this.isSelected = false;
+    this.isFavorite = (_imageFile.rank==1);
+  } // byImgFile constructor
+
+  final ImgFile _imageFile;
+  String assetName;
+  String assetPackage;
+  String title;
+  String caption;
   bool isSelected = false;
   bool isFavorite;
   String get tag => assetName; // Assuming that all asset names are unique.
@@ -37,113 +44,22 @@ class Photo {
 }
 
 class GridPhotoViewer extends StatefulWidget {
-  const GridPhotoViewer({ Key key, this.photo }) : super(key: key);
-
+  const GridPhotoViewer(this.photo);
   final Photo photo;
 
   @override
   _GridPhotoViewerState createState() => new _GridPhotoViewerState();
 }
 
-class _GridTitleText extends StatelessWidget {
-  const _GridTitleText(this.text);
-
-  final String text;
+class _GridPhotoViewerState extends State<GridPhotoViewer>  {
 
   @override
   Widget build(BuildContext context) {
-    return new FittedBox(
-      fit: BoxFit.scaleDown,
-      alignment: Alignment.centerLeft,
-      child: new Text(text),
-    );
-  }
-}
-
-class _GridPhotoViewerState extends State<GridPhotoViewer> with SingleTickerProviderStateMixin {
-  AnimationController _controller;
-  Animation<Offset> _flingAnimation;
-  Offset _offset = Offset.zero;
-  double _scale = 1.0;
-  Offset _normalizedOffset;
-  double _previousScale;
-
-  @override
-  void initState() {
-    super.initState();
-    _controller = new AnimationController(vsync: this)
-      ..addListener(_handleFlingAnimation);
-  }
-
-  @override
-  void dispose() {
-    _controller.dispose();
-    super.dispose();
-  }
-
-  // The maximum offset value is 0,0. If the size of this renderer's box is w,h
-  // then the minimum offset value is w - _scale * w, h - _scale * h.
-  Offset _clampOffset(Offset offset) {
-    final Size size = context.size;
-    final Offset minOffset = new Offset(size.width, size.height) * (1.0 - _scale);
-    return new Offset(offset.dx.clamp(minOffset.dx, 0.0), offset.dy.clamp(minOffset.dy, 0.0));
-  }
-
-  void _handleFlingAnimation() {
-    setState(() {
-      _offset = _flingAnimation.value;
-    });
-  }
-
-  void _handleOnScaleStart(ScaleStartDetails details) {
-    setState(() {
-      _previousScale = _scale;
-      _normalizedOffset = (details.focalPoint - _offset) / _scale;
-      // The fling animation stops if an input gesture starts.
-      _controller.stop();
-    });
-  }
-
-  void _handleOnScaleUpdate(ScaleUpdateDetails details) {
-    setState(() {
-      _scale = (_previousScale * details.scale).clamp(1.0, 4.0);
-      // Ensure that image location under the focal point stays in the same place despite scaling.
-      _offset = _clampOffset(details.focalPoint - _normalizedOffset * _scale);
-    });
-  }
-
-  void _handleOnScaleEnd(ScaleEndDetails details) {
-    final double magnitude = details.velocity.pixelsPerSecond.distance;
-    if (magnitude < _kMinFlingVelocity)
-      return;
-    final Offset direction = details.velocity.pixelsPerSecond / magnitude;
-    final double distance = (Offset.zero & context.size).shortestSide;
-    _flingAnimation = new Tween<Offset>(
-        begin: _offset,
-        end: _clampOffset(_offset + direction * distance)
-    ).animate(_controller);
-    _controller
-      ..value = 0.0
-      ..fling(velocity: magnitude / 1000.0);
-  }
-
-  @override
-  Widget build(BuildContext context) {
-    return new GestureDetector(
-      onScaleStart: _handleOnScaleStart,
-      onScaleUpdate: _handleOnScaleUpdate,
-      onScaleEnd: _handleOnScaleEnd,
-      child: new ClipRect(
-        child: new Transform(
-          transform: new Matrix4.identity()
-            ..translate(_offset.dx, _offset.dy)
-            ..scale(_scale),
+    return new ClipRect(
           child: new Image.network(widget.photo.assetName,
       //      package: widget.photo.assetPackage,
             fit: BoxFit.cover,
           ),
-        ),
-      ),
     );
   }
 }
@@ -151,6 +67,7 @@ class _GridPhotoViewerState extends State<GridPhotoViewer> with SingleTickerProv
 class GridDemoPhotoItem extends StatelessWidget {
   GridDemoPhotoItem({
     Key key,
+    @required this.imageFile,
     @required this.photo,
     @required this.tileStyle,
     @required this.onBannerTap
@@ -158,9 +75,9 @@ class GridDemoPhotoItem extends StatelessWidget {
         assert(tileStyle != null),
         assert(onBannerTap != null),
         super(key: key);
-
+  final ImgFile imageFile;
   final Photo photo;
-  final GridDemoTileStyle tileStyle;
+  final bool tileStyle;
   final BannerTapCallback onBannerTap; // User taps on the photo's header or footer.
 //  final BannerTapCallback onCheckTap; // User taps on the photo's header or footer.
 
@@ -174,7 +91,7 @@ class GridDemoPhotoItem extends StatelessWidget {
             body: new SizedBox.expand(
               child: new Hero(
                 tag: photo.tag,
-                child: new GridPhotoViewer(photo: photo),
+                child: new GridPhotoViewer(photo),
               ),
             ),
           );
@@ -191,7 +108,6 @@ class GridDemoPhotoItem extends StatelessWidget {
             tag: photo.tag,
             child: new Image.network(
               photo.assetName,
-           //   package: photo.assetPackage,
               fit: BoxFit.cover,
             )
         )
@@ -201,54 +117,33 @@ class GridDemoPhotoItem extends StatelessWidget {
     final IconData iconCut =  Icons.content_cut ;
     final IconData iconSelect = photo.isSelected ? Icons.check_box : Icons.check_box_outline_blank;
 
-    switch (tileStyle) {
-      case GridDemoTileStyle.imageOnly:
-        return image;
-
-      case GridDemoTileStyle.oneLine:
-        return new GridTile(
-          header: new GestureDetector(
-            onTap: () { onBannerTap(photo); },
-            child: new GridTileBar(
-              title: new _GridTitleText(photo.title),
+    if (tileStyle) {
+      return new GridTile(
+        footer: new GestureDetector(
+          onTap: () {
+            onBannerTap(photo,imageFile);
+          },
+          child: new GridTileBar(
               backgroundColor: Colors.black45,
-              leading: Row(
-                children: [
-                  new Icon(iconCut, color: Colors.white),
-                  new Icon(icon, color: Colors.white),
-                ]
-              ),
-            ),
-          ),
-          child: image,
-        );
-
-      case GridDemoTileStyle.twoLine:
-        return new GridTile(
-          footer: new GestureDetector(
-            onTap: () { onBannerTap(photo); },
-            child: new GridTileBar(
-              backgroundColor: Colors.black45,
-              title: new _GridTitleText(photo.title),
-              subtitle: new _GridTitleText(photo.caption),
+              title: Text(photo.title),
+              subtitle: Text(photo.caption),
               trailing: Row(
-                children: [
-                new Icon(iconCut, color: Colors.white),
-              new Icon(icon, color: Colors.white),
-              ])
-            ),
+                  children: [
+                    new Icon(iconCut, color: Colors.white),
+                    new Icon(icon, color: Colors.white),
+                  ])
           ),
-          child: image,
-        );
-
-      case GridDemoTileStyle.selecting:
+        ),
+        child: image,
+      );
+    } else {
         return new GridTile(
           header: new GestureDetector(
-            onTap: () { onBannerTap(photo); },
+            onTap: () { onBannerTap(photo,imageFile); },
             child: new GridTileBar(
                 backgroundColor: photo.isSelected ? Colors.black45 :Colors.black87,
-                title: new _GridTitleText(photo.title),
-                subtitle: new _GridTitleText(photo.caption),
+                title: Text(photo.title),
+                subtitle: Text(photo.caption),
                 trailing: new Icon(iconSelect, color: Colors.white),
             ),
           ),
@@ -260,8 +155,18 @@ class GridDemoPhotoItem extends StatelessWidget {
   }
 }
 
+/*****************************************************************
+ *                     Main Entry Point
+ ****************************************************************/
 class GridListDemo extends StatefulWidget {
-  const GridListDemo({ Key key }) : super(key: key);
+  ImageFilter _imageFilter;
+
+  GridListDemo();
+
+  GridListDemo.byFilter(ImageFilter initFilter) {
+    print('Started by filter');
+    _imageFilter = initFilter;
+  }
 
   static const String routeName = '/material/grid-list';
 
@@ -270,69 +175,68 @@ class GridListDemo extends StatefulWidget {
 }
 
 class GridListDemoState extends State<GridListDemo> {
-  GridDemoTileStyle _tileStyle = GridDemoTileStyle.twoLine;
+  bool _tileStyle = false;
 
-  List<Photo> photos = <Photo>[
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4192.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Omokoroa',
-      caption: 'Boat Club',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4193.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Omokoroa',
-      caption: 'Boat Club',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4194.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Omokoroa',
-      caption: 'Boat Club',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4195.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Omokoroa',
-      caption: 'Boat Club',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4196.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Tanjore',
-      caption: 'Thanjavur Temple',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4197.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Pondicherry',
-      caption: 'Salt Farm',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4198.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Chennai',
-      caption: 'Scooters',
-    ),
-    new Photo(
-      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4199.JPG',
-      assetPackage: _kGalleryAssetsPackage,
-      title: 'Chettinad',
-      caption: 'Silk Maker',
-    ),
+//  List<Photo> hardPhotos = <Photo>[
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4192.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Omokoroa',
+//      caption: 'Boat Club',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4193.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Omokoroa',
+//      caption: 'Boat Club',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4194.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Omokoroa',
+//      caption: 'Boat Club',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4195.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Omokoroa',
+//      caption: 'Boat Club',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4196.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Tanjore',
+//      caption: 'Thanjavur Temple',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4197.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Pondicherry',
+//      caption: 'Salt Farm',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4198.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Chennai',
+//      caption: 'Scooters',
+//    ),
+//    new Photo(
+//      assetName: 'http://192.168.1.251:3333/2017-01/thumbnails/DSCN4199.JPG',
+//      assetPackage: _kGalleryAssetsPackage,
+//      title: 'Chettinad',
+//      caption: 'Silk Maker',
+//    ),
+//
+//  ];
 
-  ];
-
-  void changeTileStyle(GridDemoTileStyle value) {
+  void changeTileStyle(bool value) {
     setState(() {
       _tileStyle = value;
     });
   }
   void changeSelectMode() {
     setState(() {
-      _tileStyle =  _tileStyle==GridDemoTileStyle.selecting?
-      GridDemoTileStyle.twoLine : GridDemoTileStyle.selecting;
+      _tileStyle =  !_tileStyle;
     });
   }
 
@@ -344,52 +248,34 @@ class GridListDemoState extends State<GridListDemo> {
         title: const Text('Grid list'),
         actions: <Widget>[
           new RaisedButton(onPressed: changeSelectMode,child: Text('Select'),),
-          new PopupMenuButton<GridDemoTileStyle>(
-            onSelected: changeTileStyle,
-            itemBuilder: (BuildContext context) => <PopupMenuItem<GridDemoTileStyle>>[
-              const PopupMenuItem<GridDemoTileStyle>(
-                value: GridDemoTileStyle.imageOnly,
-                child: Text('Image only'),
-              ),
-              const PopupMenuItem<GridDemoTileStyle>(
-                value: GridDemoTileStyle.oneLine,
-                child: Text('One line'),
-              ),
-              const PopupMenuItem<GridDemoTileStyle>(
-                value: GridDemoTileStyle.twoLine,
-                child: Text('Two line'),
-              ),
-            ],
-          ),
         ],
       ),
       body: new Column(
         children: <Widget>[
+          ImageFilterWidget(widget._imageFilter),
           new Expanded(
-            child: new SafeArea(
-              top: false,
-              bottom: false,
               child: new GridView.count(
                 crossAxisCount: (orientation == Orientation.portrait) ? 4 : 6,
                 mainAxisSpacing: 4.0,
                 crossAxisSpacing: 4.0,
                 padding: const EdgeInsets.all(4.0),
                 childAspectRatio: (orientation == Orientation.portrait) ? 1.0 : 1.3,
-                children: photos.map((Photo photo) {
+                children: widget._imageFilter.images.map((ImgFile imageFile) {
                   return new GridDemoPhotoItem(
-                      photo: photo,
+                      photo: Photo.byImgFile(imageFile),
+                      imageFile: imageFile,
                       tileStyle: _tileStyle,
-                      onBannerTap: (Photo photo) {
+                      onBannerTap: (photo,imageFile) {
                         setState(() {
                           photo.isSelected = !photo.isSelected; // TODO : Not both settings
                           photo.isFavorite = !photo.isFavorite;
                         });
                       }
                   );
-                }).toList(),
+                }).toList().cast<GridDemoPhotoItem>(),
               ),
             ),
-          ),
+          ImageEditorWidget(),
         ],
       ),
     );
