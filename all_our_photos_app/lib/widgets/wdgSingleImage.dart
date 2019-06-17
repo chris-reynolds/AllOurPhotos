b00d04eec375e47b2c124afc8b02fef6c8744a19
Chris Reynolds <chrisr@instantobjects.com>
2019-06-18 11:30:51 +1200

    Add photoView for pinch-zoom

--- all_our_photos_app/lib/widgets/wdgSingleImage.dart
+++ all_our_photos_app/lib/widgets/wdgSingleImage.dart
@@ -2,9 +2,38 @@ import 'package:flutter/material.dart';
 import '../dart_common/Logger.dart' as Log;
 import '../shared/aopClasses.dart';
 import '../dart_common/WidgetSupport.dart';
+import 'package:http/http.dart' as Http;
+import 'package:path/path.dart' as Path;
+import '../JpegLoader.dart';
+import 'package:photo_view/photo_view.dart';
 
 void showExif(BuildContext context,AopSnap thisSnap) async {
-  await showMessage(context,'Just me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\n');
+//  await showMessage(context,'Just me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\nJust me\n and me\nand me\n\n');
+  if (thisSnap.fileName.indexOf('helen') >= 0)
+    Log.message('checkpoint available');
+  String thisExtension = Path.extension(thisSnap.fileName).toLowerCase();
+  if (thisExtension == '.jpg') {
+//    try {
+//      Image originalImage = decodeImage(fileImageContents);
+//      thisSnap.width = originalImage.width;
+//      thisSnap.height = originalImage.height;
+//      //         bool isPortrait = (originalImage.height > originalImage.width);
+//      //  thumbnailImage = copyResize(originalImage, isPortrait ? 480 : 640);
+//    } catch (ex) {
+//      Log.error('failed to decode ${thisImageFile.path}');
+//      exit;
+//    }
+    Http.Response hr = await Http.get(thisSnap.fullSizeURL);
+    List<int> fileImageContents = hr.bodyBytes;
+    var jpegLoader = JpegLoader();
+    await jpegLoader.loadBuffer(fileImageContents);
+    if (jpegLoader.tags != null && jpegLoader.tags.length > 0) {
+      String tagResult = '';
+      jpegLoader.tags.forEach((k,v)=> tagResult+= '$k = $v \n');
+      showMessage(context, tagResult,
+          title: 'Exif for ${thisSnap.fileName}');
+    }
+  }
 
 }
 void showPhoto(BuildContext context, List<AopSnap> snapList, int index) {
@@ -16,6 +45,7 @@ void showPhoto(BuildContext context, List<AopSnap> snapList, int index) {
         title: new Text(snap.fileName + ' ' + snap.caption),
         actions: <Widget>[
           IconButton(icon: Icon(Icons.edit), onPressed: () {}),
+          IconButton(icon: Icon(Icons.rotate_90_degrees_ccw), onPressed: () {}),
           IconButton(icon: Icon(Icons.crop), onPressed: () {}),
           IconButton(icon: Icon(Icons.palette), onPressed: () {}),
           PopupMenuButton<String>(
@@ -68,9 +98,12 @@ class _SingleImageWidgetState extends State<SingleImageWidget> {
   @override
   Widget build(BuildContext context) {
     return new ClipRect(
-      child: new Image.network(
-        widget._snap.fullSizeURL,
-        fit: BoxFit.scaleDown,
+//      child: new Image.network(
+//        widget._snap.fullSizeURL,
+//        fit: BoxFit.scaleDown,
+//      ),
+      child: PhotoView(
+          imageProvider: NetworkImage(widget._snap.fullSizeURL),
 
       ),
     );
