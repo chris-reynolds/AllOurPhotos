573810cd8a3498c4f9bb8e2d101f05bc4f304ee8
Chris Reynolds <chrisr@instantobjects.com>
2019-10-08 18:29:20 +1300

    Bigger Icon, Show rotated picture, code auto format.

--- all_our_photos_app/lib/widgets/wdgPhotoTile.dart
+++ all_our_photos_app/lib/widgets/wdgPhotoTile.dart
@@ -12,18 +12,16 @@ import '../dart_common/DateUtil.dart';
 
 typedef void BannerTapCallback(AopSnap snap);
 
-
 class PhotoTile extends StatelessWidget {
-  PhotoTile({
-    Key key,
-    @required this.snapList,
-    @required this.index,
-    @required this.isSelected,
-    @required this.inSelectMode,
-    @required this.highResolution,
-    @required this.onBannerTap
-  })
-      : assert(isSelected != null ),
+  PhotoTile(
+      {Key key,
+      @required this.snapList,
+      @required this.index,
+      @required this.isSelected,
+      @required this.inSelectMode,
+      @required this.highResolution,
+      @required this.onBannerTap})
+      : assert(isSelected != null),
         assert(inSelectMode != null),
         assert(onBannerTap != null),
         super(key: key);
@@ -40,55 +38,58 @@ class PhotoTile extends StatelessWidget {
   Widget build(BuildContext context) {
     final Widget imageWidget = new GestureDetector(
         onTap: () {
-          Navigator.of(context).pushNamed('SinglePhoto',arguments:[snapList,index]); // weakly types params. yuk.
+          Navigator.of(context)
+              .pushNamed('SinglePhoto', arguments: [snapList, index]); // weakly types params. yuk.
         },
         child: new Container(
             decoration: BoxDecoration(color: Color.fromRGBO(0, 0, 0, 0.05)),
             key: new Key(snap.thumbnailURL),
 //            tag: snap.fileName,
-            child: new Image.network(
-              snap.thumbnailURL,
+            child: Transform.rotate(
+              angle:snap.angle,
+              child: Image.network(
+                snap.thumbnailURL,
 //              highResolution ? snap.fullSizeURL : snap.thumbnailURL,
-              fit: BoxFit.scaleDown,
-            )
-        )
-    );
+                fit: BoxFit.scaleDown,
+              ),
+            )));
 
     final IconData icon = Icons.star;
     final IconData iconSelect = isSelected ? Icons.check_box : Icons.check_box_outline_blank;
-    String descriptor = '${formatDate(snap.takenDate,format:'dmmm yy')} ${snap.caption??snap.location??""}';
+    String descriptor =
+        '${formatDate(snap.takenDate, format: 'dmmm yy')} ${snap.caption ?? snap.location ?? ""}';
 //    if (descriptor == null || descriptor.length == 0)
 //      descriptor = '${formatDate(snap.takenDate,format:'dmmm yy')} ${snap.location??''}';
     if (!inSelectMode) {
       return new GridTile(
-          header: new GestureDetector(
-              onTap: () {
-                onBannerTap(snap);
-              },
-              child: new GridTileBar(
+        header: new GestureDetector(
+          onTap: () {
+            onBannerTap(snap);
+          },
+          child: new GridTileBar(
               title: Text(descriptor, style: TextStyle(color: Colors.black)),
-          //subtitle: (snap.location != null) ? Text(snap.location??'') : Container(),
-          trailing: Row(
-              children: [
-                new Icon(icon, color: filterColors[snap.ranking], size: 30.0),
-              ])
-      ),
-    ),
-    child: imageWidget,
-    );
+              //subtitle: (snap.location != null) ? Text(snap.location??'') : Container(),
+              trailing: Row(children: [
+                new Icon(icon, color: filterColors[snap.ranking], size: 40.0),
+              ])),
+        ),
+        child: imageWidget,
+      );
     } else {
-    return new GridTile(
-    header: new GestureDetector(
-    onTap: () { onBannerTap(snap); },
-    child: new GridTileBar(
+      return new GridTile(
+        header: new GestureDetector(
+          onTap: () {
+            onBannerTap(snap);
+          },
+          child: new GridTileBar(
 //            backgroundColor: isSelected ? Colors.black45 :Colors.black26,
-    title: Text(descriptor,style:TextStyle(color:Colors.black)),
+            title: Text(descriptor, style: TextStyle(color: Colors.black)),
 //            subtitle: Text(formatDate(snap.takenDate,format:'mmm-yyyy'),style:TextStyle(color:Colors.black)),
-    trailing: new Icon(iconSelect, color: Colors.black),
-    ),
-    ),
-    child: imageWidget,
-    );
+            trailing: new Icon(iconSelect, color: Colors.black),
+          ),
+        ),
+        child: imageWidget,
+      );
     }
   }
 }
