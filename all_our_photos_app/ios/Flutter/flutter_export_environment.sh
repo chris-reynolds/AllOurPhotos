2200915717a1feb5760f2d0c50eb651bb1d7baef
Chris Reynolds <chrisr@instantobjects.com>
2020-03-26 10:21:52 +1300

    ios flutter noise

--- all_our_photos_app/ios/Flutter/flutter_export_environment.sh
+++ all_our_photos_app/ios/Flutter/flutter_export_environment.sh
@@ -5,6 +5,7 @@ export "FLUTTER_APPLICATION_PATH=/Users/chrisreynolds/Desktop/projects/AllOurPho
 export "FLUTTER_TARGET=/Users/chrisreynolds/Desktop/projects/AllOurPhotos/all_our_photos_app/lib/aopAppMain.dart"
 export "FLUTTER_BUILD_DIR=build"
 export "SYMROOT=${SOURCE_ROOT}/../build/ios"
+export "OTHER_LDFLAGS=$(inherited) -framework Flutter"
 export "FLUTTER_FRAMEWORK_DIR=/Users/chrisreynolds/development/flutter/bin/cache/artifacts/engine/ios"
 export "FLUTTER_BUILD_NAME=0.0.54"
 export "FLUTTER_BUILD_NUMBER=0.0.54"
