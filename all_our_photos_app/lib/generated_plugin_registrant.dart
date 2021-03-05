36530ac418010d5eeeb87026020b8bedd74ae0cd
Chris Reynolds <chrisr@instantobjects.com>
2021-03-06 09:12:48 +1300

    all_our_photos mass commit prior to Flutter2

--- all_our_photos_app/lib/generated_plugin_registrant.dart
+++ all_our_photos_app/lib/generated_plugin_registrant.dart
@@ -5,12 +5,14 @@
 // ignore: unused_import
 import 'dart:ui';
 
+import 'package:file_picker/src/file_picker_web.dart';
 import 'package:image_picker_for_web/image_picker_for_web.dart';
 
 import 'package:flutter_web_plugins/flutter_web_plugins.dart';
 
 // ignore: public_member_api_docs
 void registerPlugins(PluginRegistry registry) {
+  FilePickerWeb.registerWith(registry.registrarFor(FilePickerWeb));
   ImagePickerPlugin.registerWith(registry.registrarFor(ImagePickerPlugin));
   registry.registerMessageHandler();
 }
