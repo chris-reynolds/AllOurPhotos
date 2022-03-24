bab99db8f06739e8ae3fccc6174ce10b7721f5e7
Chris Reynolds <chrisr@instantobjects.com>
2022-03-24 17:38:34 +1300

    Add gesture detector for the whole filter widget

--- all_our_photos_app/lib/widgets/wdgImageFilter.dart
+++ all_our_photos_app/lib/widgets/wdgImageFilter.dart
@@ -80,85 +80,94 @@ class ImageFilterWidgetState extends State<ImageFilterWidget> {
   //  @override
   Widget build(BuildContext context) {
 //    print('building ImageFilter with changeMode=$changeMode and refreshRequired=${_imageFilter.refreshRequired}');
-    return new Center(
-        child: !changeMode
-            ? Row(
-                children: [
-                  IconButton(icon: Icon(Icons.first_page),onPressed: (){onMonthMove(-1);},tooltip: 'Back 1 month', iconSize: 36,),
-                  Text(
-                    '  Filter : ${_imageFilter.searchText} ${Utils.formatDate(_imageFilter.fromDate, format: 'd-mmm-yyyy')}' +
-                        ' upto ${Utils.formatDate(_imageFilter.toDate, format: 'd-mmm-yyyy')}',
-                  ),
-                  IconButton(icon: Icon(Icons.last_page),onPressed: (){onMonthMove(1);},tooltip: 'Advance 1 month',iconSize: 36,),
-                  Expanded(child: Text('')),
-                  IconButton(
-                    icon: Icon(Icons.search),
-                    onPressed: () {
-                      setState(() {
-                        if (changeMode == false) changeMode = true;
-                      }); // of setState
-                    }, // of onPressed
-                  ),
-                ],
-              )
-            : new Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                crossAxisAlignment: CrossAxisAlignment.center,
-                children: <Widget>[
-                  Row(mainAxisAlignment: MainAxisAlignment.center, children: [
-                    qText('Search:'),
-                    Flexible(
-                        child: TextField(
-                      controller: textController,
+    return GestureDetector(
+      behavior: HitTestBehavior.translucent,
+      onTap: () {
+        setState(() {
+          if (changeMode == false) changeMode = true;
+        }); // of setState
+      },
+      child: new Center(
+          child: !changeMode
+              ? Row(
+                  children: [
+                    IconButton(icon: Icon(Icons.first_page),onPressed: (){onMonthMove(-1);},tooltip: 'Back 1 month', iconSize: 36,),
+                    Text(
+                      '  Filter : ${_imageFilter.searchText} ${Utils.formatDate(_imageFilter.fromDate, format: 'd-mmm-yyyy')}' +
+                          ' upto ${Utils.formatDate(_imageFilter.toDate, format: 'd-mmm-yyyy')}',
+                    ),
+                    IconButton(icon: Icon(Icons.last_page),onPressed: (){onMonthMove(1);},tooltip: 'Advance 1 month',iconSize: 36,),
+                    Expanded(child: Text('')),
+                    IconButton(
+                      icon: Icon(Icons.filter),
+                      onPressed: () {
+                        setState(() {
+                          if (changeMode == false) changeMode = true;
+                        }); // of setState
+                      }, // of onPressed
+                      tooltip: 'change search criteria',
+                    ),
+                  ],
+                )
+              : new Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: <Widget>[
+                    Row(mainAxisAlignment: MainAxisAlignment.center, children: [
+                      qText('Search:'),
+                      Flexible(
+                          child: TextField(
+                        controller: textController,
 //                  onChanged: onSearchTextChanged,
-                      style: Theme.of(context).textTheme.bodyText2,
-                    )),
-                    qText('Rank:'),
-                    IconButton(
-                        icon: Icon(_rankIcon(_imageFilter.getRank(1)),
-                            color: filterColors[1], size: 40.0),
-                        onPressed: () {
-                          toggleRank(1);
-                        }),
-                    IconButton(
-                        icon: Icon(_rankIcon(_imageFilter.getRank(2)),
-                            color: filterColors[2], size: 40.0),
-                        onPressed: () {
-                          toggleRank(2);
-                        }),
-                    IconButton(
-                        icon: Icon(_rankIcon(_imageFilter.getRank(3)),
-                            color: filterColors[3], size: 40.0),
-                        onPressed: () {
-                          toggleRank(3);
-                        }),
-                  ]),
-                  Row(
+                        style: Theme.of(context).textTheme.bodyText2,
+                      )),
+                      qText('Rank:'),
+                      IconButton(
+                          icon: Icon(_rankIcon(_imageFilter.getRank(1)),
+                              color: filterColors[1], size: 40.0),
+                          onPressed: () {
+                            toggleRank(1);
+                          }),
+                      IconButton(
+                          icon: Icon(_rankIcon(_imageFilter.getRank(2)),
+                              color: filterColors[2], size: 40.0),
+                          onPressed: () {
+                            toggleRank(2);
+                          }),
+                      IconButton(
+                          icon: Icon(_rankIcon(_imageFilter.getRank(3)),
+                              color: filterColors[3], size: 40.0),
+                          onPressed: () {
+                            toggleRank(3);
+                          }),
+                    ]),
+                    Row(
 //            mainAxisAlignment: MainAxisAlignment.
-                      children: [
-                        qText('From '),
-                        TapDateWidget(_imageFilter.fromDate, (changedDate) {
-                          setState(() {
-                            _imageFilter.fromDate = changedDate;
-                          });
-                        }), // of TapDateWidget
-                        qText('upto '),
-                        TapDateWidget(_imageFilter.toDate, (changedDate) {
-                          setState(() {
-                            _imageFilter.toDate = changedDate;
-                          });
-                        }), // of TapDateWidget
-                        Expanded(
-                          child: Row(
-                            mainAxisAlignment: MainAxisAlignment.end,
-                            children: [
-                              IconButton(
-                                  icon: Icon(Icons.refresh, size: 40.0), onPressed: onRefresh),
-                            ],
-                          ),
-                        )
-                      ]), // Date filter
-                ],
-              ));
+                        children: [
+                          qText('From '),
+                          TapDateWidget(_imageFilter.fromDate, (changedDate) {
+                            setState(() {
+                              _imageFilter.fromDate = changedDate;
+                            });
+                          }), // of TapDateWidget
+                          qText('upto '),
+                          TapDateWidget(_imageFilter.toDate, (changedDate) {
+                            setState(() {
+                              _imageFilter.toDate = changedDate;
+                            });
+                          }), // of TapDateWidget
+                          Expanded(
+                            child: Row(
+                              mainAxisAlignment: MainAxisAlignment.end,
+                              children: [
+                                IconButton(
+                                    icon: Icon(Icons.refresh, size: 40.0), onPressed: onRefresh),
+                              ],
+                            ),
+                          )
+                        ]), // Date filter
+                  ],
+                )),
+    );
   }
 }
