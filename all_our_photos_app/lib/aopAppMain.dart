a2d2dfa3edf96b78c3a51787608da7cf680d70e1
Chris Reynolds <chrisr@instantobjects.com>
2022-10-10 18:50:05 +1300

    deployed to moth ipads with updated title

--- all_our_photos_app/lib/aopAppMain.dart
+++ all_our_photos_app/lib/aopAppMain.dart
@@ -8,7 +8,7 @@ import 'screens/scSinglePhoto.dart';
 import 'screens/scDBFix.dart';
 import 'screens/scLaunchWithLogin.dart';
 
-const VERSION = 'All Our Photos 3 Oct22.v1 - linter';
+const VERSION = 'All Our Photos 10 Oct22.v1';
 
 void main() {
 
