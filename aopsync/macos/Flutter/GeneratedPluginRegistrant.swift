02a9bee999e4d7754cdfb14abc3e177781b7e72b
Chris Reynolds <chrisr@instantobjects.com>
2024-01-09 15:13:00 +1300

    aopsync macos build files

--- aopsync/macos/Flutter/GeneratedPluginRegistrant.swift
+++ aopsync/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -5,10 +5,10 @@
 import FlutterMacOS
 import Foundation
 
-import path_provider_macos
-import photo_manager
+import path_provider_foundation
+import shared_preferences_foundation
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
-  PhotoManagerPlugin.register(with: registry.registrar(forPlugin: "PhotoManagerPlugin"))
+  SharedPreferencesPlugin.register(with: registry.registrar(forPlugin: "SharedPreferencesPlugin"))
 }
